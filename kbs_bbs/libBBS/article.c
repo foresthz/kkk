dcee1cf4b6bc5478d1bc334f21e7d4d281d5be3a
fancy <fancy@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2011-05-23 14:59:42 +0000

    jiangjun2000: fix 精华区转出的带附件且没来源的帖子被修改后的bug
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@10703 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/libBBS/article.c
+++ kbs_bbs/libBBS/article.c
@@ -2105,8 +2105,18 @@ int add_edit_mark(char *fname, int mode, char *title, session_t* session)
                 added = 1;
             }
             fputs(buf, out);
-        } else
+        } else {
+            /* 考虑从精华区转出的、没有来源且带附件的帖子 */
+            if (!added) {
+                added = 1;
+                now = time(0);
+                if (mode & 1)
+                    fprintf(out, "\033[36m※ 修改:·%s 于 %20.20s 修改本信·[FROM: %s]\033[m\n", session->currentuser->used, ctime_r(&now, timebuf) + 4, SHOW_USERIP(session->currentuser, session->fromhost));
+                else
+                    fprintf(out, "\033[36m※ 修改:·%s 于 %20.20s 修改本文·[FROM: %s]\033[m\n", session->currentuser->used, ctime_r(&now, timebuf) + 4, SHOW_USERIP(session->currentuser, session->fromhost));
+            }
             put_attach(fp, out, asize);
+        }
     }
     if (!added) {
         now = time(0);
