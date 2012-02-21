388684d7238a23e3d9daa5e19bf5fd438cd0ae4b
jiangjun <jiangjun@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2012-02-21 12:18:45 +0000

    让你们输入0
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@11089 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/src/bm.c
+++ kbs_bbs/src/bm.c
@@ -541,7 +541,7 @@ int modify_denytime(time_t *denytime, int *autofree)
             if (days > 1) /* 至少需要1天 */
                 days--;
         } else if (ch>='0' && ch<='9') { /* 直接输入日期，可输入三位数字 */
-            if (!start)
+            if (!start && ch!='0')
                 days = ch - '0';
             else {
                 int day = ch - '0';
