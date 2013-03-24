844c9b85a028884676f74e4db00c822ba619b485
jiangjun <jiangjun@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2013-03-24 15:03:10 +0000

    web下不显示CURRBM的目录
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@11587 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/php/phpbbs_announce.c
+++ kbs_bbs/php/phpbbs_announce.c
@@ -645,6 +645,10 @@ PHP_FUNCTION(bbs_read_ann_dir)
         if (strcmp(r_bm, "BMS")==0) {
             if (HAS_PERM(getCurrentUser(), PERM_BOARDS) && (seespecial!=0))
                 cansee = true;
+        } else if (strcmp(r_bm, "CURRBM")==0) {
+            const struct boardheader *bh;
+            if ((bh=getbcache(bname))!=NULL && chk_currBM(bh->BM, getCurrentUser()) && (seespecial!=0))
+                cansee = true;
         } else if (strcmp(r_bm, "SYSOPS")==0) {
             if (HAS_PERM(getCurrentUser(), PERM_SYSOP) && (seespecial!=0))
                 cansee = true;
