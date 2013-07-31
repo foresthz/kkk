2a7e791ea85dbc63598c95c773ca2115866a4421
windinsn <windinsn@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2013-07-31 08:43:40 +0000

    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@11639 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/libBBS/libnewmsg.c
+++ kbs_bbs/libBBS/libnewmsg.c
@@ -672,12 +672,6 @@ int new_msg_send(struct new_msg_handle *sender, struct new_msg_handle *incept, s
 	if (msg->size > NEW_MSG_MAX_SIZE || msg->size <= 0)
 		return -5;
 		
-	// windinsn, 2013.07.26 filter	
-	if(check_badword_str(msg->msg, msg->size, getSession())){
-		newbbslog(BBSLOG_USER, "want to send filtered message to '%s' ", incept->user);	
-		return 0; // 有关键字的直接拦截
-	}
-		
 	flag=0;
 	if (NULL!=attachment) {
 		flag |= NEW_MSG_MESSAGE_ATTACHMENT;
@@ -731,8 +725,15 @@ int new_msg_send(struct new_msg_handle *sender, struct new_msg_handle *incept, s
 	sprintf(message.name, "与 %s 的对话", sender->user);	
 	strncpy(message.msg.key, sender->user, IDLEN+1);
 	message.msg.key[IDLEN+1]=0;
-	if (new_msg_create(incept, &message, attachment_file)<0) {
-		return -12;
+	
+	// windinsn, 2013.07.26 filter	
+	if(check_badword_str(msg->msg, msg->size, getSession())){
+		newbbslog(BBSLOG_USER, "want to send filtered message to '%s' ", incept->user);	
+		// 有关键字的直接拦截
+	} else {
+		if (new_msg_create(incept, &message, attachment_file)<0) {
+			return -12;
+		}
 	}
 	
 	message.msg.flag|=NEW_MSG_MESSAGE_SEND;
