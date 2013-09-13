a7adf6487ac46a5b9e895613b0cf4de3161931ab
jiangjun <jiangjun@de4889a5-8ad4-4bf3-bd87-57997c21d515>
2013-09-13 16:20:15 +0000

    检查驻版可读楼主时判断可靠一些
    git-svn-id: http://svn.kcn.cn/repos/kbs/trunk@11684 de4889a5-8ad4-4bf3-bd87-57997c21d515

--- kbs_bbs/libBBS/libmember.c
+++ kbs_bbs/libBBS/libmember.c
@@ -2205,7 +2205,7 @@ int member_read_perm(const struct boardheader *bh, struct fileheader *fh, struct
         return 1;
     if (fh!=NULL && (POSTFILE_BASENAME(fh->filename)[0]=='Z' || is_top(fh, bh->filename) || isowner(user, fh))) /* 置顶或作者 */
         return 1;
-    if (fh!=NULL && get_ent_from_id_ext(DIR_MODE_NORMAL, fh->groupid, bh->filename, &orig) && isowner(user, &orig)) /* 楼主 */
+    if (fh!=NULL && get_ent_from_id_ext(DIR_MODE_NORMAL, fh->groupid, bh->filename, &orig)>0 && isowner(user, &orig)) /* 楼主 */
         return 1;
 
     bzero(&member, sizeof(struct board_member));
